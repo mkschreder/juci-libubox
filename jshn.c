6a03e5e9d802d85776a5f7008e198a9ea98dda29
Martin Schröder <mkschreder.uk@gmail.com>
2015-06-20 03:55:51 +0200

    Modified the build scripts

--- jshn.c
+++ jshn.c
@@ -13,11 +13,8 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-#ifdef JSONC
-        #include <json.h>
-#else
-        #include <json/json.h>
-#endif
+
+#include <json.h>
 
 #include <string.h>
 #include <stdlib.h>
